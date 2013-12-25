91fbb0539d6d66dc9f1076a19a6ad355a3445632
Z. Bornheimer <z@chary.me>
2013-12-25 18:56:39 -0500

    Updated paths

--- idf.pl
+++ idf.pl
@@ -30,7 +30,8 @@ perl idf.pl store <file relative to ~/>
 =cut
 
 my %target;
-$target{'rc.lua'} = $ENV{HOME} . '/.config/awesome/';
+$target{'lua'} = $ENV{HOME} . '/.config/awesome/';
+$target{'.vim'} = $ENV{HOME} . '/.vim/colors/';
 
 my @vimFiles = qw/ Tomorrow-Night.vim Tomorrow-Night-Bright.vim /;
 
